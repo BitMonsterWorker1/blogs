fde2c8ec8f04eb451e9c92dccc9fdedaf538f562
MAX <30717231+BitMonsterWorker1@users.noreply.github.com>
2017-12-04 00:38:12 -0500

    post users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
 before_action :find_post, only: [:show, :edit, :destroy]
-
-
+before_action :authenticate_user!, only: [:new, :edit, :destroy]
+before_action :is_admin, only: [:edit, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
-  def new
-    @post = Post.new
-  end
+
 
 def show
-
 end
 
+def new
+  @post = Post.new
+end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
@@ -24,20 +24,16 @@ end
     end
   end
 
-  def edit
-
-  end
-
-  def update
-
-if  @post.update(post_params)
-  redirect_to @post
-else
-  render :edit
-
-
+def edit
 end
-  end
+
+def update
+ if  @post.update(post_params)
+  redirect_to @post
+ else
+  render :edit
+ end
+end
 
 def destroy
 @post.destroy
@@ -45,11 +41,19 @@ redirect_to root_path
 end
 
   private
-def find_post
-  @post = Post.find(params[:id])
-end
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :user)
   end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+
+  def is_admin
+   unless current_user == @post.user
+     redirect_to @post
+  end
+ end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :title, length:  {minimum: 4}
-  validates :body, length:  {minimum: 25}
+  validates :body, length:  {minimum: 5}
 end
